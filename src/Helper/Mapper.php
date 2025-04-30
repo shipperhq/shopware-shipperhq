1a0517446b9863e7aa7a6fa3cdd5c756e5b2e8e7
wsajosh <joshua.stewart@shipperhq.com>
2025-04-30 15:32:27 +0100

    SHQ23-4820 Update copyrights

--- src/Helper/Mapper.php
+++ src/Helper/Mapper.php
@@ -4,7 +4,7 @@
  * ShipperHQ
  *
  * @category ShipperHQ
- * @package ShipperHQ_Calendar
+ * @package SHQ\RateProvider
  * @copyright Copyright (c) 2025 Zowta LTD and Zowta LLC (http://www.ShipperHQ.com)
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @author ShipperHQ Team sales@shipperhq.com
